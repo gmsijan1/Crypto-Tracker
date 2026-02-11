02f2cdd78b754b6068cfd7fbb18e2eadf84949b2
Sijan <gmsijan1@gmail.com>
2026-02-11 11:55:02 -0500

    feat: add Firebase auth (email & Google) and coin sorting

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+// main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import CryptoWrapper from "./CryptoContext";
-import "./style.css";
+import CryptoWrapper from "./CryptoContext.jsx";
+import { AuthProvider } from "./AuthContext.jsx";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CryptoWrapper>
-      <App />
-    </CryptoWrapper>
+    <AuthProvider>
+      <CryptoWrapper>
+        <App />
+      </CryptoWrapper>
+    </AuthProvider>
   </StrictMode>,
 );
